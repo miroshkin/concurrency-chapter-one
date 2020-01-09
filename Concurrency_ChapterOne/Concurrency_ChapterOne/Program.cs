a777171145ddd2ff960a50769ad10365b3b579b1
Anton Miroshkin <anton.miroshkin@gmail.com>
2020-01-09 12:58:47 +0300

    Added project

---
+++ Concurrency_ChapterOne/Concurrency_ChapterOne/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace Concurrency_ChapterOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Concurrency - Chapter One!");
+            
+            Thread thread = new Thread(PrintNumbersWithDelay);
+            thread.Start();
+
+            PrintNumbers();
+        }
+
+        static void PrintNumbers()
+        {
+            Console.WriteLine("Starting...");
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void PrintNumbersWithDelay()
+        {
+            Console.WriteLine("Starting...");
+            for (int i = 1; i < 10; i++)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
